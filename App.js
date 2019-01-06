6a093fd261c1792d7fe448621a2a1ed0f4b7d3e6
Kasia_Wrzesniak <wrzesniak.k@gmail.com>
2019-01-06 23:52:18 +0100

    Code the basic layer

--- App.js
+++ App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-
-import AppNavigator from './navigation/AppNavigator';
+import { StyleSheet, KeyboardAvoidingView, Text, TextInput } from 'react-native';
 
 export default class App extends React.Component {
 
   render() {
     return (
-        <AppLoading
-          startAsync={this._loadResourcesAsync}
-          onError={this._handleLoadingError}
-          onFinish={this._handleFinishLoading}
+      <KeyboardAvoidingView 
+        style={styles.container}
+        behavior="padding"
+        >
+         <Text style={[styles.blue, styles.largeText]}> Barcelona </Text>
+         <Text style={styles.smallText}>Rain</Text>
+         <Text style={styles.smallText}>26 degrees</Text>
+
+         <TextInput
+          style={[styles.textInput]}
         />
-      );
+      </KeyboardAvoidingView>
+    )
   }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  blue: {
+    color: 'blue',
+  },
+  textStyle: {
+    textAlign: 'center',
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+  textInput: {
+    backgroundColor: '#666',
+    color: 'white',
+    height: 40,
+    width: 300,
+    marginTop: 20,
+    marginHorizontal: 20,
+    paddingHorizontal: 10,
+    alignSelf: 'center',
   },
 });
